3099e4ed97b69c874cf6b0c76a6e1a24e8d64178
ihkaru <ihza2karunia@gmail.com>
2023-04-28 19:36:58 +0700

    fix redirector

--- dist/spa/assets/redirector.8e451f83.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index.a6ae9af0.js";var t=r(({urlPath:o,redirect:e})=>{if(o!="/login"&&o=="/"){console.log(o);return}});export{t as default};
---
+++ dist/spa/assets/redirector.bae0ade9.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.108efa36.js";var i=t(({urlPath:o,redirect:r})=>{if(o!="/login"&&o=="/"){r({path:"/login"});return}});export{i as default};
