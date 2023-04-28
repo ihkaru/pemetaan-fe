beccad07b7f0bb9d69aa6e48f07cfd2692266e45
ihkaru <ihza2karunia@gmail.com>
2023-04-28 18:54:27 +0700

    publish build

---
+++ dist/spa/assets/MainLayout.6f416ff7.js
@@ -0,0 +1,1 @@
+import{_ as o,o as a,c as t,a as r,r as _}from"./index.c9203439.js";const n={},c={id:"mainLayout",class:"scrollbar-hide"};function s(i,l){const e=_("router-view");return a(),t("div",c,[r(e)])}var d=o(n,[["render",s]]);export{d as default};
---
+++ dist/spa/assets/redirector.a865babf.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.c9203439.js";var e=a(async({urlPath:o,redirect:r})=>{o=="/"&&r({path:"/login"})});export{e as default};
