81d500058ec6e6c6e441bfe8ea29a93d5b6991cf
ihkaru <ihza2karunia@gmail.com>
2023-04-30 01:23:46 +0700

    add social login ui

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,23 +6,21 @@
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup=""></script>
 
 <style lang="scss">
-  #indexPage {
-    width: 100vw;
-    height: 100vh;
+#indexPage {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .card {
+    width: 100%;
+    height: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-    .card{
-      width: 100%;
-      height: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
   }
+}
 </style>
--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,38 +1,48 @@
 <template>
-    <div id="loginPage">
-        <q-card class="q-pa-xl q-gutter-y-md column login-form" data-aos="fade-left">
-            <p class="text-center app-title">Mempawah App</p>
-            <p class="text-center title">Login</p>
-            <q-input outlined label="Email" />
-            <q-input outlined label="Password" />
-            <q-toggle v-model="rememberMe" label="Remember me"/>
-            <q-btn to="/map" color="primary" icon="login" label="login" @click="onClick" />
-        </q-card>
-    </div>
+  <div id="loginPage">
+    <q-card class="q-pa-xl q-gutter-y-md column login-form" data-aos="zoom-out">
+      <p class="text-center app-title">Mempawah App</p>
+      <p class="text-center title">Login</p>
+      <q-input v-model="email" type="email" outlined label="Email" />
+      <q-input v-model="password" outlined type="password" label="Password" />
+      <q-toggle v-model="rememberMe" label="Remember me" />
+      <q-btn
+        to="/map"
+        color="primary"
+        icon="login"
+        label="Masuk"
+        @click="onClick"
+      />
+      <div class="tw-w-full text-center">atau masuk dengan akun</div>
+      <q-btn icon="img:./icons/google.svg" label="Google" />
+      <q-btn icon="img:./icons/bps.svg" label="SOBAT BPS" />
+    </q-card>
+  </div>
 </template>
 <script setup>
-    import {ref} from "vue";
-    const rememberMe = ref(false);
+import { ref } from "vue";
+const rememberMe = ref(false);
+const email = ref("");
+const password = ref("");
 </script>
 
 <style lang="scss">
-    #loginPage {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .login-form{
-            width: 450px;
-            .app-title {
-                font-weight: bold;
-                font-size: large;
-            }
-            .title {
-                font-size: x-large;
-                font-weight: bold;
-            }
-        }
-        
+#loginPage {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-form {
+    width: 450px;
+    .app-title {
+      font-weight: bold;
+      font-size: large;
     }
+    .title {
+      font-size: x-large;
+      font-weight: bold;
+    }
+  }
+}
 </style>
