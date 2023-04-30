bb17388cef7c336b3b2d47042664acc019f1995d
ihkaru <ihza2karunia@gmail.com>
2023-04-30 18:47:53 +0700

    publish pwa

---
+++ dist/pwa/assets/redirector.09661f49.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.4fdf042b.js";var i=t(({urlPath:o,redirect:r})=>{if(o!="/login"&&o=="/"){r({path:"/login"});return}});export{i as default};
--- dist/pwa/assets/redirector.d7ece533.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.9f48036b.js";var i=t(({urlPath:o,redirect:r})=>{if(o!="/login"&&o=="/"){r({path:"/login"});return}});export{i as default};
